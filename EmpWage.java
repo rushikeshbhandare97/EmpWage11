a00ca9936eaf872b9e8a437b7d1290fa13091782
technology-97 <rushikeshbhandare789@gmail.com>
2021-07-15 17:43:29 +0600

    Added:EmpWage   is added

--- EmpWage.java
+++ EmpWage.java
@@ -4,12 +4,23 @@ public class EmpWage {
 {
 
      int IS_FULL_TIME = 1;
+     int EMP_RATE_PER_HOUR =20 ;
+     int empHrs = 0;
+     int empWage= 0;
      double  empCheck = Math.floor (Math.random () * 10 ) % 2;
      if( empCheck == IS_FULL_TIME)
+        empHrs = 8;
 
-       System.out.println("Employee is Present");
+       
      else
-      System.out.println("Employee is Absent");
+         empHrs = 0;
+      empWage = empHrs * EMP_RATE_PER_HOUR;
+      System.out.println ("Emp Wage: " +empWage );
+      
+
+
+
+
 }
 
 }
